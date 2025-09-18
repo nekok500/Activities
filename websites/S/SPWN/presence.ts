bcc4d085624894251b8aec57507333c28c640d2b
nekok500 <nekok500@proton.me>
2025-09-18 11:02:06 +0900

    style(SPWN): remove comment

--- websites/S/SPWN/presence.ts
+++ websites/S/SPWN/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 })
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
-enum ActivityAssets { // Other default assets can be found at index.d.ts
+enum ActivityAssets {
   Logo = 'https://i.imgur.com/LvbXO0a.png',
 }
 
